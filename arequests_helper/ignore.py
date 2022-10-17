a11755cdafcac99c383f415693c6fd6800659139
Vitalii Haruk <dropharuk>
2022-10-17 11:50:51 +0300

    hghjg

--- arequests_helper/ignore.py
+++ arequests_helper/ignore.py
@@ -2,4 +2,4 @@ import requests
 import asyncio
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
